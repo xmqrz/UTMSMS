da29b29896156b51c17e99b5537461cc3683e77c
kohlihui <k.l.hui2003@gmail.com>
2024-06-23 23:19:49 +0800

    update

--- UTMSMS  4.1/src/LoginSystem.java
+++ UTMSMS  4.1/src/LoginSystem.java
@@ -62,7 +62,7 @@ public class LoginSystem {
         users = new User[]{
                 new User("A22EC0193", coursesUser1, gradesUser1, student1, "User1.txt"),
                 new User("A22EC0194", coursesUser2, gradesUser2, student2, "User2.txt"),
-                new User("A22EC0195", coursesUser3, gradesUser3, student3, "User3.txt")
+                new User("A22EC0059", coursesUser3, gradesUser3, student3, "User3.txt")
         };
 
         // Initialize Finance array
